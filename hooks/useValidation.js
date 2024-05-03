2f687205d3be430edabe34405477d53379de2ebe
icedhwa <162160689+icedhwa@users.noreply.github.com>
2024-05-03 21:01:58 +0300

    Add files via upload

---
+++ hooks/useValidation.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+
+const useValidation = (initialState, validation, actionFn) => {
+    const [values, storeValues] = useState(initialState);
+    const [errors, storeErrors] = useState({});
+    const [submitForm, storeSubmitForm] = useState(false);
+
+    useEffect(() => {
+        if (submitForm) {
+            const success = Object.keys(errors).length === 0;
+
+            if (success) {
+                actionFn();
+            }
+            storeSubmitForm(false);
+        }
+    }, [errors]);
+
+    const updateValues = (newValues) => {
+        storeValues(newValues);
+    };
+
+    const handleChange = (e) => {
+        storeValues({
+            ...values,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const validationError = validation(values);
+        storeErrors(validationError);
+        storeSubmitForm(true);
+    };
+
+    const handleBlur = () => {
+        const validationError = validation(values);
+        storeErrors(validationError);
+    };
+
+    return {
+        values,
+        errors,
+        updateValues,
+        handleSubmit,
+        handleChange,
+        handleBlur,
+    };
+};
+
+export default useValidation;
